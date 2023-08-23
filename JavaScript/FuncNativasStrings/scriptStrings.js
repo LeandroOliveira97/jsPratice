975aff9b1de0c70a35a11eb7a05177aeda87e832
Leandro <leandronotebook18@gmail.com>
2023-08-22 23:59:58 -0300

    Fim dia 22!

--- JavaScript/FuncNativasStrings/scriptStrings.js
+++ JavaScript/FuncNativasStrings/scriptStrings.js
@@ -2,3 +2,11 @@ document.writeln("Leandro Elenildo Silva de Oliveira".length);
 document.writeln("Jorge Sant Ana".charAt(8));
 document.writeln("Jorge Sant Ana".toLocaleLowerCase());
 document.writeln("Jorge Sant Ana".replace("Sant Ana", "Elenildo"));
+document.writeln("<br> <br>");
+
+var x = Math.random();
+
+document.writeln(x);
+
+
+
