15ea38ba047edfb7afe5be29ebcfbcdd2369c77a
Leandro <leandronotebook18@gmail.com>
2023-08-30 00:06:19 -0300

    Manipulacao de Arrays

---
+++ JavaScript/Desafio Cadeira/scriptDesafioCadeira.js
@@ -0,0 +1,38 @@
+var arrayObjetos = Array();
+
+
+function atualizarTa(arrayObj) {
+    document.getElementById('ta_listagem').value = arrayObj;
+}
+
+
+function inserirObjetoArray() {
+
+    if (document.getElementById('in_objetos').value != '') {gi
+        var objeto = document.getElementById('in_objetos').value;
+
+
+        if (!arrayObjetos.includes(objeto)) {
+            arrayObjetos.push(objeto);
+            console.log(arrayObjetos);
+            atualizarTa(arrayObjetos);
+            document.getElementById('in_objetos').value = '';
+
+        } else {
+            alert('O objeto já foi inserido anteriormente!');
+        }
+    } else {
+        alert('Valor inválido!');
+    }
+
+
+
+
+}
+
+function ordenar() {
+
+    console.log(arrayObjetos.sort());
+    atualizarTa(arrayObjetos);
+
+}
